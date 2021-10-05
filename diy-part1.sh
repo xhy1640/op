f279d126792c2e88270ccade150655613e1e8031
xhy1640 <67546886+xhy1640@users.noreply.github.com>
2021-10-05 18:05:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-openclash
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-pushbot
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-ssr-plus
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
